1cf97e5ee94d7bc22c35f54daea7e5d8828f07c5
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2023-08-22 14:42:45 +0700

    get campaign detail endpoint

--- main.go
+++ main.go
@@ -35,6 +35,8 @@ func main() {
 	campaignHandler := handler.NewCampaignHandler(campaignService)
 
 	router := gin.Default()
+	router.Static("/images", "./images")
+
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
@@ -43,6 +45,7 @@ func main() {
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
 
 	api.GET("/campaigns", campaignHandler.GetCampaigns)
+	api.GET("/campaign/:id", campaignHandler.GetCampaign)
 
 	router.Run()
 }
